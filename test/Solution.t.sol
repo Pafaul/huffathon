b32109224a643bd1c7a4a22d293e356fef092224
Pavel Mikhailov <paulmihailov2013@gmail.com>
2023-09-28 20:21:11 +0300

    init solution

--- test/Solution.t.sol
+++ test/Solution.t.sol
@@ -30,6 +30,8 @@ contract SolutionTest is Test, Utils {
         console.log("data:");
         console.logBytes(data);
 
+        console.log(gasReport());
+
         bool solved = CREATOR.verify(solutionAddress);
 
         require(solved, "Solution failed");
